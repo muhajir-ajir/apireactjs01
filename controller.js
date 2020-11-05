3d879188c2f0ba80eb407e9731d64ce9f9be806c
muhajir-ajir <ghozanemozir@gmail.com>
2020-11-05 13:33:51 -0500

    berhasil menampilkan data

--- controller.js
+++ controller.js
@@ -4,5 +4,15 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function (req, res) {
-    response.ok("aplikasi Rest API berjalan");
+    response.ok("aplikasi Rest API berjalan", res);
+};
+
+exports.listMahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (err, rows, fileds) {
+        if (err) {
+            connection.log(err);
+        } else {
+            response.ok(rows, res)
+        }
+    });
 };
